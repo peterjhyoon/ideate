f8ede348561ec014968c15d1f06c1f77e8a72b3b
Peter Yoon <peyoon@ucsd.edu>
2024-05-10 11:37:23 -0700

    login progress

--- client/src/components/user/Login.jsx
+++ client/src/components/user/Login.jsx
@@ -1,8 +1,16 @@
+import LoginInput from "../ui/LoginInput";
 
 function Login() {
     return (
-        <>
-        </>
+        <div className="container mx-auto text-center">
+            <h1 className="text-4xl py-8 font-bold mx-auto">Login to Your Account</h1>
+            <LoginInput id="username" 
+                input_placeholder={"Email"}
+            />
+            <LoginInput id="password" 
+                input_placeholder={"Password"}
+            />
+        </div>
     )
 }
 
--- client/src/pages/Accounts.jsx
+++ client/src/pages/Accounts.jsx
@@ -1,8 +1,16 @@
+import Login from "../components/user/Login";
+import CreateAcc from "../components/user/CreateAcc";
 
 function Accounts() {
     return (
-        <>
-        </>
+        <div className="flex w-full h-screen">
+            <div className="w-2/3" id="login-comp">
+                <Login />
+            </div>
+            <div className="w-1/3 bg-purple-700 text-white" id="create-acc-comp">
+                {/* add createacc comp here */}
+            </div>
+        </div>
     )
 }
 
