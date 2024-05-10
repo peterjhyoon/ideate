682b5fa80237db8d216d2fe8e00bb2cc37702aef
Peter Yoon <peyoon@ucsd.edu>
2024-05-10 03:24:09 -0700

    project detail window struct

--- client/src/components/ProjectDetail.jsx
+++ client/src/components/ProjectDetail.jsx
@@ -1,8 +1,13 @@
 
 const ProjectDetail = ({ project, onClose }) => {
     return (
-        <>
-        </>
+        <div className="container border border-black rounded-full">
+            <button onClick={onClose}>Close</button> {/* Close button */}
+            <h2>{project.title}</h2>
+            <p><strong>Team:</strong> {project.project_name}</p>
+            <p><strong>Location:</strong> {project.location}</p>
+            <p><strong>Description:</strong> {project.description}</p>
+        </div>
     )
 }
 
